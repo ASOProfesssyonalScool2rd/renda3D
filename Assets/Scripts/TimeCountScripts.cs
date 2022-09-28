071c05366722dadfdf2c469654abcf85e0e5c563
uchidaEditorial <kouei28poiou@icloud.com>
2022-09-28 12:08:05 +0900

    Cheanged 2022/09/28

--- Assets/Scripts/TimeCountScripts.cs
+++ Assets/Scripts/TimeCountScripts.cs
@@ -1,21 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeCountScripts : MonoBehaviour
 {
     public int countdownMinutes = 30;
-    
+
+    private float countdownsecons;
+
+    private Text Timetext;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Timetext.GetComponent<Text>();
+        countdownsecons = countdownMinutes * 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countdownsecons -= Time.deltaTime;
+        var span = new TimeSpan(0, 0, (int)countdownsecons);
+        Timetext.text = span.ToString(@"mm\:ss");
+        if (countdownsecons <= 0)
+        {
+            
+        }
     }
     
 }
